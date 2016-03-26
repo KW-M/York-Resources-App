beaa9cc8e4a5af1e7f50f23b9b08145995306359
kw_m <worcester-moorek2018@york.org>
2016-03-26 18:39:15 +0000

    AutoCommit

--- js/Controllers/mainController.js
+++ js/Controllers/mainController.js
@@ -1,5 +1,5 @@
-                                                                            /*global app*//*global angular*//*global gapi*/
-app.controller('ApplicationController', ['$scope', '$mdDialog', '$window', '$mdSidenav', '$mdMedia', 'authorizationService', 'GoogleDriveService', function ($scope, $mdDialog, $window, $mdSidenav, $mdMedia, authorizationService, GoogleDriveService) {
+/*global app*/ /*global angular*/ /*global gapi*/
+app.controller('ApplicationController', ['$scope', '$mdDialog', '$window', '$mdSidenav', '$mdMedia', 'authorizationService', 'GoogleDriveService', function($scope, $mdDialog, $window, $mdSidenav, $mdMedia, authorizationService, GoogleDriveService) {
     $scope.Posts = [];
     $scope.searchTerm = '';
     $scope.searchedPosts = [];
@@ -8,15 +8,15 @@ app.controller('ApplicationController', ['$scope', '$mdDialog', '$window', '$mdS
         SidebarIsOpen: false //not used
     };
 
-    $scope.signIn = function () { //called by the signIn button click
+    $scope.signIn = function() { //called by the signIn button click
         loginProcedure(authorizationService.authorizePopup());
     };
 
-    $scope.toggleSidebar = function () { //called by the top left toolbar menu button
+    $scope.toggleSidebar = function() { //called by the top left toolbar menu button
         $mdSidenav('left').toggle();
     };
 
-    $scope.helpDialog = function () { //called by the top right toolbar help button
+    $scope.helpDialog = function() { //called by the top right toolbar help button
         $mdDialog.show({
             templateUrl: 'templates/html/help.html',
             parent: angular.element(document.body),
@@ -25,7 +25,7 @@ app.controller('ApplicationController', ['$scope', '$mdDialog', '$window', '$mdS
         });
     };
 
-    $scope.newPost = function () { //called by the bottom right plus/add resource button
+    $scope.newPost = function() { //called by the bottom right plus/add resource button
         $mdDialog.show({
             templateUrl: 'templates/html/newPost.html',
             controller: ['$scope', '$mdDialog', 'GoogleDriveService', newPostController],
@@ -41,44 +41,46 @@ app.controller('ApplicationController', ['$scope', '$mdDialog', '$window', '$mdS
 
     function loginProcedure(response) {
         //handles the 'response' promise
-        response.then(function (response) {
+        response.then(function(response) {
                 $scope.loginStatus = response;
                 GoogleDriveService.initiateAuthLoadDrive($scope.initiateDrive)
-            }).catch(function (error) {
+            }).catch(function(error) {
                 if (error.error_subtype !== undefined && error.error_subtype === "access_denied") {
                     showLoginButton();
-                } else if (error.error !== undefined && error.error === "access_denied") {
+                }
+                else if (error.error !== undefined && error.error === "access_denied") {
                     showLoginButton();
-                } else {
+                }
+                else {
                     console.log(error);
                 }
             })
-            //called to show the login button (& hide the loading spinner) 
+            //called to show the login button (& hide the loading spinner)
         function showLoginButton() {
             $('#login_spinner').fadeOut(200,
-                function () {
+                function() {
                     $('#auth_button').fadeIn(200);
                 });
         };
     };
 
-    $scope.initiateDrive = function () {
-        GoogleDriveService.batchRequest().then(function (response) {
-            // make a formatter to convert the http response aray into body array
-            $scope.Posts = arrayOfPosts;
+    $scope.initiateDrive = function() {
+        GoogleDriveService.batchRequest().then(function(response) {
+            $scope.Posts = response.result;
             $scope.$apply();
         });
     }
 
-    $window.pushPosts = function (response) {
+    $window.pushPosts = function(response) {
 
     }
 
-    $window.onscroll = function (event) { //called whenever the window scrolls
+    $window.onscroll = function(event) { //called whenever the window scrolls
         var yScroll = $window.pageYOffset;
         if (yScroll >= 100) {
             $('#speed-dial-container').slideDown(300);
-        } else {
+        }
+        else {
             $('#speed-dial-container').slideUp(300);
         }
         $('#Masonry_Container').masonry({
@@ -89,10 +91,10 @@ app.controller('ApplicationController', ['$scope', '$mdDialog', '$window', '$mdS
         });
     };
 
-    $window.loginSilent = function (response) {
+    $window.loginSilent = function(response) {
         loginProcedure(authorizationService.authorizeSilent());
     };
-            }]);
+}]);
 
 //called by the google client api when it loads (must be outside the controller)
 function gClientLoaded() {
