3bca820c4c41039167dc2bc9d4e22213325abdd8
kw_m <worcester-moorek2018@york.org>
2016-03-26 21:59:40 +0000

    AutoCommit

--- js/Services/googleDriveService.js
+++ js/Services/googleDriveService.js
@@ -32,6 +32,7 @@ app.service('GoogleDriveService', ['$q', function ($q) {
     this.batchRequest = function () { //do this one
         var promiseArray = [];
         return (self.getListOfFlies().then(function (fileArray) {
+            console.log(fileArray)
             for (var count = 0; count < fileArray.result.files.length; count++) {
                 var file = fileArray.result.files[count];
                 var fileRequest = self.getDriveFileContent(file.id);
