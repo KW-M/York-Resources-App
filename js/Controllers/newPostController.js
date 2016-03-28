f97260d5e5b92faf9cca12601aed4867c1b51d03
kw_m <worcester-moorek2018@york.org>
2016-03-28 19:51:29 +0000

    AutoCommit

--- js/Controllers/newPostController.js
+++ js/Controllers/newPostController.js
@@ -11,53 +11,55 @@ function newPostController($scope, $mdDialog, GoogleDriveService) {
     $scope.readOnly = false;
     $scope.classSearch = "";
     $scope.courses = ["English III", "Spanish I", "Chemistry", "AP Biology", "Geometry", "Algebra II", "Physics", "calc AB", "Chinese I"];
+    else {
+        $scope.Link = "http://" + $scope.Link
+    }
     var type = function() {
         if ($scope.Link === '') {
             return ('NoLink');
         }
         else {
-        
-            if ($scope.Link.match((/(?:http|https):\/\/.{3,}/)) {
-                    if ($scope.Link.match(/\/(?:d|file|folder)\/([-\w]{25,})\//)) {
-                        return ('gDrive');
-                    }
-                    else {
-                        return ('Link');
-                    }
+            if ($scope.Link.match(/(?:http|https):\/\/.{3,}/)) {
+                if ($scope.Link.match(/\/(?:d|file|folder)\/([-\w]{25,})\//)) {
+                    return ('gDrive');
                 }
-            };
-        };
-        $scope.submit = function() {
-            GoogleDriveService.getUserInfo().then(function(userInfo) {
-                console.log(userInfo.result);
-                var description = document.querySelector('#DescriptionTxt').textContent;
-                var response = ({
-                    "Type": type,
-                    "Title": $scope.Title,
-                    "Creator": {
-                        "Name": userInfo.result.user.displayName,
-                        "Email": userInfo.result.user.emailAddress,
-                        "ClassOf": '2018',
-                    },
-                    "CreationDate": new Date(),
-                    "UpdateDate": new Date(),
-                    "Tags": $scope.Tags,
-                    "Description": description,
-                    "Class": {
-                        "Name": "Name Of Class",
-                        "Teacher": "name of teacher"
-                    },
-                    "Link": $scope.Link,
-                    "FileId": "If present, the link to the resource of the post (haven't setup ui drive integration yet)",
-                    "ImageURL": "https://api.pagelr.com/capture?uri=" + $scope.Link + "&width=400&height=260&key=Ca7GOVe9BkGefE_rvwN2Bw",
-                    "LikeUsers": [],
-                });
-                console.log(response);
-                console.log('sending...');
-                GoogleDriveService.sendDriveFile(response, $scope.Title).then(function(reply) {
-                    console.log(reply.result);
-                    $scope.close();
-                });
+                else {
+                    return ('Link');
+                }
+            }
+        }
+    };
+    $scope.submit = function() {
+        GoogleDriveService.getUserInfo().then(function(userInfo) {
+            console.log(userInfo.result);
+            var description = document.querySelector('#DescriptionTxt').textContent;
+            var response = ({
+                "Type": type,
+                "Title": $scope.Title,
+                "Creator": {
+                    "Name": userInfo.result.user.displayName,
+                    "Email": userInfo.result.user.emailAddress,
+                    "ClassOf": '2018',
+                },
+                "CreationDate": new Date(),
+                "UpdateDate": new Date(),
+                "Tags": $scope.Tags,
+                "Description": description,
+                "Class": {
+                    "Name": "Name Of Class",
+                    "Teacher": "name of teacher"
+                },
+                "Link": $scope.Link,
+                "FileId": "If present, the link to the resource of the post (haven't setup ui drive integration yet)",
+                "ImageURL": "https://api.pagelr.com/capture?uri=" + $scope.Link + "&width=400&height=260&key=Ca7GOVe9BkGefE_rvwN2Bw",
+                "LikeUsers": [],
             });
-        };
-    }
+            console.log(response);
+            console.log('sending...');
+            GoogleDriveService.sendDriveFile(response, $scope.Title).then(function(reply) {
+                console.log(reply.result);
+                $scope.close();
+            });
+        });
+    };
+}
