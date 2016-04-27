6054851a2a2134dcdf3ed7aedaf64a5ce9f561db
kw_m <worcester-moorek2018@york.org>
2016-04-27 02:00:58 +0000

    AutoCommit

--- js/Services/googleDriveService.js
+++ js/Services/googleDriveService.js
@@ -98,7 +98,7 @@ app.service('GoogleDriveService', ['$q', function($q) {
 
     this.multiRequest = function() { //do this one
         var promiseArray = [];
-        return (self.getListOfFlies().then(RateLimit(function(fileArray) {
+        return (RateLimit(self.getListOfFlies().then(function(fileArray) {
             console.log(fileArray)
             for (var count = 0; count < fileArray.result.files.length; count++) {
                 var file = fileArray.result.files[count];
@@ -107,7 +107,7 @@ app.service('GoogleDriveService', ['$q', function($q) {
                 console.log(promiseArray);
             }
             return ($q.all(promiseArray));
-        }, 2000)))
+        }))
     };
 
 
