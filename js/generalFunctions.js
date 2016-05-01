92dd76be68000a2f2a7f4de9711de002f60546a6
kw_m <worcester-moorek2018@york.org>
2016-05-01 17:18:15 +0000

    AutoCommit

--- js/generalFunctions.js
+++ js/generalFunctions.js
@@ -8,7 +8,28 @@ function formatArrayResponse (rawArrayResponse,idArray) {
       }
     return (arrayOfPosts);
 }
+app.factory('P', function($q) {
+  var queue = [], timer = null;
 
+  function processQueue() {
+    var item = queue.shift();
+    if (item)
+      fn.apply(item.context, item.arguments);
+    if (queue.length === 0)
+      clearInterval(timer), timer = null;
+  }
+
+  return function limited() {
+    queue.push({
+      context: context || this,
+      arguments: [].slice.call(arguments)
+    });
+    if (!timer) {
+      processQueue();  // start immediately on the first invocation
+      timer = setInterval(processQueue, delay);
+    }
+  }
+});
 function RateLimit(fn, delay, context) {
   var queue = [], timer = null;
 
