7f515ccf25d2c8874ab755ebf9e556e485665b81
kw_m <worcester-moorek2018@york.org>
2016-05-05 01:04:07 +0000

    AutoCommit

--- js/Controllers/newPostController.js
+++ js/Controllers/newPostController.js
@@ -1,5 +1,6 @@
 /* we don't define the "new post controller" here because it was alredy
    defined by the $md-dialog in the newPost function on mainController.   */
+   alert("ahh ta da")
 function newPostController($scope, $mdDialog, GoogleDriveService) {
     console.log($scope.globals);
     $scope.close = function() {
