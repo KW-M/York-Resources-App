601d9a8e492747a2c1fb2bd6a68485850d68e00b
kw_m <worcester-moorek2018@york.org>
2016-05-06 23:56:33 +0000

    AutoCommit

--- directives/directives.js
+++ directives/directives.js
@@ -34,3 +34,22 @@ app.directive('addBarTop', function () {
         templateUrl: 'directives/html/addBarTop.html'
     };
 });
+
+app.directive('getPosts', function() {
+  return {
+    restrict: 'AE',
+    replace: true,
+    template: '<p style="background-color:{{color}}">Hello World',
+    link: function(scope, elem, attrs) {
+      elem.bind('click', function() {
+        elem.css('background-color', 'white');
+        scope.$apply(function() {
+          scope.color = "white";
+        });
+      });
+      elem.bind('mouseover', function() {
+        elem.css('cursor', 'pointer');
+      });
+    }
+  };
+});
