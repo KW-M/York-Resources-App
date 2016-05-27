5c7a2dab67b12782e961b55b0c77858e469da4ae
kw_m <worcester-moorek2018@york.org>
2016-05-27 00:00:38 +0000

    AutoCommit

--- js/generalFunctions.js
+++ js/generalFunctions.js
@@ -1,65 +1,62 @@
-  var theQueue = [],
-    timer = null;
+  var theQueue = [];
+  var timer = null;
 
-function formatArrayResponse(rawArrayResponse, idArray) {
-  var arrayOfPosts = [];
-  for (var i = 0; i < rawArrayResponse.length; i++) {
-    if (idArray != null) {
-      rawArrayResponse[i].result.ID = idArray[i];
+  function formatArrayResponse(rawArrayResponse, idArray) {
+    var arrayOfPosts = [];
+    for (var i = 0; i < rawArrayResponse.length; i++) {
+      if (idArray != null) {
+        rawArrayResponse[i].result.ID = idArray[i];
+      }
+      arrayOfPosts.push(rawArrayResponse[i].result);
     }
-    arrayOfPosts.push(rawArrayResponse[i].result);
+    return (arrayOfPosts);
   }
-  return (arrayOfPosts);
-}
 
 
   function processTheQueue() {
     var item = theQueue.shift();
     if (item) {
       var thePromise = item.Promise
-      console.log(thePromise)
       thePromise.then(item.Action);
     }
-    if (queue.length === 0){
+    if (queue.length === 0) {
       clearInterval(timer), timer = null;
     }
   }
 
   // Take a promise.  Queue 'action'.  On 'action' faulure, run 'error' and continue.
-   function queue (promise, action, error) {
-    theQueue.push(
-      {
-        Promise: promise,
-        Action: action,
-        Err: error
-      }
-    );
+  function queue(promise, action, error) {
+    theQueue.push({
+      Promise: promise,
+      Action: action,
+      Err: error
+    });
     if (!timer) {
       processTheQueue(); // start immediately on the first invocation
-      timer = setInterval(processTheQueue, 500);
+      timer = setInterval(processTheQueue, 200);
     }
   };
 
-function RateLimit(fn, delay, context) {
-  var queue = [],
-    timer = null;
+  function RateLimit(fn, delay, context) {
+    var queue = [],
+      timer = null;
 
-  function processQueue() {
-    var item = queue.shift();
-    if (item)
-      fn.apply(item.context, item.arguments);
-    if (queue.length === 0)
-      clearInterval(timer), timer = null;
-  }
+    function processQueue() {
+      var item = queue.shift();
+      if (item)
+        fn.apply(item.context, item.arguments);
+      if (queue.length === 0)
+        clearInterval(timer), timer = null;
+    }
 
-  return function limited() {
-    queue.push({
-      context: context || this,
-      arguments: [].slice.call(arguments)
-    });
-    if (!timer) {
-      processQueue(); // start immediately on the first invocation
-      timer = setInterval(processQueue, delay);
+    return function limited() {
+      queue.push({
+        context: context || this,
+        arguments: [].slice.call(arguments)
+      });
+      if (!timer) {
+        processQueue(); // start immediately on the first invocation
+        timer = setInterval(processQueue, delay);
+      }
     }
   }
-}
