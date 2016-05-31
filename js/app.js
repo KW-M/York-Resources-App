47a22b74bc97cd4a8fc4b010393d8cd072146917
kw_m <worcester-moorek2018@york.org>
2016-05-31 03:57:17 +0000

    AutoCommit

--- js/app.js
+++ js/app.js
@@ -9,13 +9,13 @@ app.config(function ($mdThemingProvider, $mdIconProvider, $routeProvider, $locat
         .when("/",{
             templateUrl: "templates/html/all-posts.html",
         })
-        .when("my-posts",{
+        .when("/my-posts",{
             templateUrl: "templates/html/my-posts.html",
         })
-        .when("flaged",{
+        .when("/flaged",{
             templateUrl: "templates/html/flaged.html",
         })
-        .when("liked",{
+        .when("/liked",{
             templateUrl: "templates/html/most-liked.html",
         })
         .otherwise({
