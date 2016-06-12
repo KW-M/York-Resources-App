3416967d025df2d840d110472c08c10e77c6d754
kw_m <worcester-moorek2018@york.org>
2016-06-12 23:33:54 +0000

    AutoCommit

--- js/Controllers/mainController.js
+++ js/Controllers/mainController.js
@@ -280,7 +280,7 @@ app.controller('ApplicationController', dependancies.concat([function($scope, $m
    $scope.angularGridOptions = {
      gridWidth: 250,
      infiniteScroll: $scope.getFiles(),
-     scrollContainer: content_container,
+     scrollContainer: 'content_container',
      pageSize: 1.5,
      performantScroll:false,
      gutterSize: 12,
