4bcd6ef32177a336f4871817147ad2b479c3fbed
kw_m <worcester-moorek2018@york.org>
2016-07-02 19:48:11 +0000

    AutoCommit

--- js/Controllers/mainController.js
+++ js/Controllers/mainController.js
@@ -21,9 +21,7 @@ app.controller('ApplicationController', dependancies.concat([function($scope, $m
 var xhr = new XMLHttpRequest();
 xhr.open("GET", "https://crossorigin.me/https://api.pagelr.com/capture/javascript?uri=www.amazon.com&width=400&height=260&ads=0&format=jpg&maxage=32000000&key=Ca7GOVe9BkGefE_rvwN2Bw");
 xhr.onload = function (e) {
-   console.log(e);
    console.log(this);
-   console.log(this.responseText);
    var imagey = document.createElement('img');
    //imagey.src = imageUrl;
 };
