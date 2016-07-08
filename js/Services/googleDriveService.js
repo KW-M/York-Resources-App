be84e8fedc188f79461e9272e11760962f3665cb
kw_m <worcester-moorek2018@york.org>
2016-07-08 16:12:18 +0000

    AutoCommit

--- js/Services/googleDriveService.js
+++ js/Services/googleDriveService.js
@@ -19,7 +19,7 @@ app.service('GoogleDriveService', ['$q', function($q) {
             pageSize: pageSize,
             pageToken: pageToken,
             q: "'0B5NVuDykezpkbUxvOUMyNnRsUGc' in parents and trashed = false",
-            fields: 'files(appProperties,properties,contentHints/thumbnail,createdTime,description,fullFileExtension,id,modifiedTime,name,owners(displayName,emailAddress),permissions(displayName,emailAddress)),nextPageToken',
+            fields: 'files(name,id,modifiedTime,appProperties,properties,contentHints/thumbnail,createdTime,description,fullFileExtension,owners(displayName,emailAddress),permissions(displayName,emailAddress)),nextPageToken',
         }));
     };
     
