bb25113f1df76460eb18145c9c1ffe40d3d0b9e9
kw_m <worcester-moorek2018@york.org>
2016-07-12 19:51:15 +0000

    AutoCommit

--- js/Controllers/mainController.js
+++ js/Controllers/mainController.js
@@ -229,11 +229,14 @@ app.controller('ApplicationController', dependancies.concat([function($scope, $m
       queue(GoogleDriveService.getListOfFlies('', $scope.nextPageToken, 12), function(fileList) {
          $scope.nextPageToken = fileList.result.nextPageToken;
          console.log(fileList);
-         for (var item = 0; item < fileList.result.files.length; item++) {
+         for (var item = 0; item = fileList.result.files.length; item++) {
+            console.log(fileList.result.files.length);
             fileList.result.files[item] = $scope.formatPost(fileList.result.files[item]);
-            $scope.$apply(function() {
-                $scope.allPosts = $scope.allPosts.concat(fileList.result.files);
-            });
+            if (item = 11){
+               $scope.$apply(function() {
+                  $scope.allPosts = $scope.allPosts.concat(fileList.result.files);
+               });
+            }
          }
       });
    }
