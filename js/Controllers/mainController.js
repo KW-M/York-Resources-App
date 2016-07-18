f98852cad113f72ff4380c7ec258252f46621574
kw_m <worcester-moorek2018@york.org>
2016-07-18 05:13:57 +0000

    AutoCommit

--- js/Controllers/mainController.js
+++ js/Controllers/mainController.js
@@ -448,7 +448,7 @@ app.controller('ApplicationController', dependancies.concat([function($scope, $m
       else {
          $mdDialog.show($mdDialog.alert({
             title: 'Uh Oh.',
-            textContent: 'One of your posts has been flagged within the past two weeks.\n To unlock the ability to unflag posts, make sure none of your posts get flagged this week.',
+            htmlContent: '<p style="margin: 0px">One of your posts has been flagged within the past two weeks.</p><p>To unlock the ability to unflag posts, make sure none of your posts get flagged this week.</p>',
             ok: 'Ok'
          }));
       }
