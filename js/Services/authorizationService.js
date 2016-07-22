eafd0d895e4112b4bbebc16ce8b27757a5b7509c
kw_m <worcester-moorek2018@york.org>
2016-07-22 18:07:45 +0000

    AutoCommit

--- js/Services/authorizationService.js
+++ js/Services/authorizationService.js
@@ -4,13 +4,27 @@ app.service('authorizationService', ['GoogleDriveService', '$q', authService]);
 //The function used in the authorizationService service also called by the google api framwork when it loads.
 function authService(GoogleDriveService, $q) {
     var self = this;
-    var client_id ='475444543746-e3r02g1o1o71kliuoohah04ojqbmo22e.apps.googleusercontent.com';
-    var scopes =  ['https://www.googleapis.com/auth/drive']
-    
+    var clientId = '475444543746-e3r02g1o1o71kliuoohah04ojqbmo22e.apps.googleusercontent.comD';
+    var apiKey = 'YOUR API KEY';
+    var scopes = 'https://www.googleapis.com/auth/drive';
+
     this.initilize = function(callback) {
         //gapi.client.setApiKey(apiKey);
+        gapi.auth2.init({
+            client_id: clientId,
+            scope: scopes
+        }).then(function() {
+            // Listen for sign-in state changes.
+            gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+
+            // Handle the initial sign-in state.
+            updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+
+            signinButton.addEventListener("click", handleSigninClick);
+            signoutButton.addEventListener("click", handleSignoutClick);
+        });
     }
-    
+
     this.buildAuthRequest = function(immediateMode) {
         var promise = $q.defer();
         var request = {
@@ -53,7 +67,7 @@ function authService(GoogleDriveService, $q) {
 
 
     this.loginPopup = function() {
-        return( gapi.auth2.init({
+        return (gapi.auth2.init({
             client_id: client_id,
             scope: scopes,
             hosted_domain: "york.org",
