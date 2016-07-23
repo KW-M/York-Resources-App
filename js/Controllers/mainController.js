3fa5193a6811c93024ed4a1b29a6f2778a362f88
kw_m <worcester-moorek2018@york.org>
2016-07-23 15:47:52 +0000

    AutoCommit

--- js/Controllers/mainController.js
+++ js/Controllers/mainController.js
@@ -559,7 +559,7 @@ app.controller('ApplicationController', dependancies.concat([function($scope, $m
 
    //-event watchers---------
 
-   $scope.$watch('searchTxt', $scope.gotoRoute());
+   $scope.$watch('searchTxt', $scope.gotoRoute($scope.searchTxt));
 
    content_container.onscroll = function(event) {
       //called whenever the content_container scrolls
