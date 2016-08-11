799e8889fd5c186dca32877dbdd72ede0b760dcb
kw_m <worcester-moorek2018@york.org>
2016-08-11 04:42:15 +0000

    AutoCommit

--- js/Controllers/mainController.js
+++ js/Controllers/mainController.js
@@ -554,7 +554,7 @@ app.controller('ApplicationController', dependancies.concat([function($scope, $m
          $mdDialog.cancel();
       };
    }
-   
+
    $scope.closeDialog = function() {
       console.log('closing dialog')
       $mdDialog.hide();
@@ -610,23 +610,23 @@ app.controller('ApplicationController', dependancies.concat([function($scope, $m
    }
 
    function handleUserPrefsSheet() {
-      queue(GoogleDriveService.getUserSettings($scope.myInfo.Email, true), function(spreadsheetRow) {
+      queue(GoogleDriveService.getUserSettings('$scope.myInfo.Email', true), function(spreadsheetRow) {
          console.log(spreadsheetRow);
          document.dispatchEvent(new Event('sheetPrefsLoaded'));
          getUserList();
       }, function(Error) {
          console.log(Error);
       });
-      
-      function getUserList () {
+
+      function getUserList() {
          queue(GoogleDriveService.getUserSettings("A2:B", false), function(spreadsheetRow) {
-         $scope.userList = spreadsheetRow.result.values
-         console.log(spreadsheetRow);
-      }, function(Error) {
-         console.log(Error);
-      });
+            $scope.userList = spreadsheetRow.result.values
+            console.log(spreadsheetRow);
+         }, function(Error) {
+            console.log(Error);
+         });
       }
-      
+
       $scope.getFiles("");
    }
 
