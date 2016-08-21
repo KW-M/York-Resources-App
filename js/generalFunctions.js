d8db0b9b1601638299bd4774169d2371031ddd55
kw_m <worcester-moorek2018@york.org>
2016-08-21 15:45:42 +0000

    AutoCommit

--- js/generalFunctions.js
+++ js/generalFunctions.js
@@ -65,6 +65,12 @@
     }
     return (arrayOfPosts);
   }
+  
+  function addDays(date, days) {
+    var result = new Date(date);
+    result.setDate(result.getDate() + days);
+    return result;
+}
 
   // function RateLimit(fn, delay, context) {
   //   var queue = [],
@@ -93,85 +99,99 @@
 
 
   /*
-   * rfc3339date.js version 0.1.3
-   * Copyright (c) 2010 Paul GALLAGHER http://tardate.com
-   * Licensed under the MIT license:
-   * http://www.opensource.org/licenses/mit-license.php
-   */
+ * rfc3339date.js version 0.1.3
+ *
+ * Adds ISO 8601 / RFC 3339 date parsing to the Javascript Date object.
+ * Copyright (c) 2010 Paul GALLAGHER http://tardate.com
+ * Licensed under the MIT license:
+ * http://www.opensource.org/licenses/mit-license.php
+ * Usage:
+ *   var d = Date.parseISO8601( "2010-07-20T15:00:00Z" );
+ *   var d = Date.parse( "2010-07-20T15:00:00Z" );
+ *   var d = new Date().toRFC3339LocaleString();  =>  "2010-07-25T19:51:31.427+08:00"
+ */
 
-  /*
-   * Date.parseRFC3339
-   * extend Date with a method parsing ISO8601 / RFC 3339 date strings.
-   * Usage: var d = Date.parseRFC3339( "2010-07-20T15:00:00Z" );
-   */
+Number.prototype.toPaddedString = function(len , fillchar) {
+  var result = this.toString();
+  if(typeof(fillchar) == 'undefined'){ fillchar = '0' };
+  while(result.length < len){ result = fillchar + result; };
+  return result;
+}
 
-  Date.prototype.parseRFC3339 = function(dString) {
-    var utcOffset, offsetSplitChar;
-    var offsetMultiplier = 1;
-    var dateTime = dString.split("T");
-    var date = dateTime[0].split("-");
-    var time = dateTime[1].split(":");
-    var offsetField = time[time.length - 1];
-    var offsetString;
-    offsetFieldIdentifier = offsetField.charAt(offsetField.length - 1);
-    if (offsetFieldIdentifier == "Z") {
-      utcOffset = 0;
-      time[time.length - 1] = offsetField.substr(0, offsetField.length - 2);
-    }
-    else {
-      if (offsetField[offsetField.length - 1].indexOf("+") != -1) {
-        offsetSplitChar = "+";
-        offsetMultiplier = 1;
+Date.prototype.toRFC3339UTCString = function(supressFormating , supressMillis){
+  var dSep = ( supressFormating ? '' : '-' );
+  var tSep = ( supressFormating ? '' : ':' );
+  var result = this.getUTCFullYear().toString();
+  result += dSep + (this.getUTCMonth() + 1).toPaddedString(2);
+  result += dSep + this.getUTCDate().toPaddedString(2);
+  result += 'T' + this.getUTCHours().toPaddedString(2);
+  result += tSep + this.getUTCMinutes().toPaddedString(2);
+  result += tSep + this.getUTCSeconds().toPaddedString(2);
+  if((!supressMillis)&&(this.getUTCMilliseconds()>0)) result += '.' + this.getUTCMilliseconds().toPaddedString(3);
+  return result + 'Z';
+}
+
+Date.prototype.toRFC3339LocaleString = function(supressFormating , supressMillis){
+  var dSep = ( supressFormating ? '' : '-' );
+  var tSep = ( supressFormating ? '' : ':' );
+  var result = this.getFullYear().toString();
+  result += dSep + (this.getMonth() + 1).toPaddedString(2);
+  result += dSep + this.getDate().toPaddedString(2);
+  result += 'T' + this.getHours().toPaddedString(2);
+  result += tSep + this.getMinutes().toPaddedString(2);
+  result += tSep + this.getSeconds().toPaddedString(2);
+  if((!supressMillis)&&(this.getMilliseconds()>0)) result += '.' + this.getMilliseconds().toPaddedString(3);
+  var tzOffset = -this.getTimezoneOffset();
+  result += ( tzOffset<0 ? '-' : '+' )
+  result += (tzOffset/60).toPaddedString(2);
+  result += tSep + (tzOffset%60).toPaddedString(2);
+  return result;
+}
+
+Date.parseRFC3339 = function(dString){
+  if (typeof dString != 'string') return;
+  var result;
+  var regexp = /(\d\d\d\d)(-)?(\d\d)(-)?(\d\d)(T)?(\d\d)(:)?(\d\d)?(:)?(\d\d)?([\.,]\d+)?($|Z|([+-])(\d\d)(:)?(\d\d)?)/i;
+  var d = dString.match(new RegExp(regexp));
+  if (d) {
+    var year = parseInt(d[1],10);
+    var mon = parseInt(d[3],10) - 1;
+    var day = parseInt(d[5],10);
+    var hour = parseInt(d[7],10);
+    var mins = ( d[9] ? parseInt(d[9],10) : 0 );
+    var secs = ( d[11] ? parseInt(d[11],10) : 0 );
+    var millis = ( d[12] ? parseFloat(String(1.5).charAt(1) + d[12].slice(1)) * 1000 : 0 );
+    if (d[13]) {
+      result = new Date(0);
+      result.setUTCFullYear(year);
+      result.setUTCMonth(mon);
+      result.setUTCDate(day);
+      result.setUTCHours(hour);
+      result.setUTCMinutes(mins);
+      result.setUTCSeconds(secs);
+      result.setUTCMilliseconds(millis);
+      if (d[13] && d[14]) {
+        var offset = (d[15] * 60)
+        if (d[17]) offset += parseInt(d[17],10);
+        offset *= ((d[14] == '-') ? -1 : 1);
+        result.setTime(result.getTime() - offset * 60 * 1000);
       }
-      else {
-        offsetSplitChar = "-";
-        offsetMultiplier = -1;
-      }
-      offsetString = offsetField.split(offsetSplitChar);
-      time[time.length - 1] == offsetString[0];
-      offsetString = offsetString[1].split(":");
-      utcOffset = (offsetString[0] * 60) + offsetString[1];
-      utcOffset = utcOffset * 60 * 1000;
+    } else {
+      result = new Date(year,mon,day,hour,mins,secs,millis);
     }
-    this.setTime(Date.UTC(date[0], date[1] - 1, date[2], time[0], time[1], time[2]) + (utcOffset * offsetMultiplier));
-    return this;
+  }
+  return result;
+};
 
-  };
-
-  /*
-   * Number.prototype.toPaddedString
-   * Number instance method used to left-pad numbers to the specified length
-   * Used by the Date.prototype.toRFC3339XXX methods
-   */
-
-  Number.prototype.toPaddedString = function(len, fillchar) {
-    var result = this.toString();
-    if (typeof(fillchar) == 'undefined') {
-      fillchar = '0'
-    };
-    while (result.length < len) {
-      result = fillchar + result;
-    };
+if (typeof Date.parse != 'function') {
+  Date.parse = Date.parseRFC3339;
+} else {
+  var oldparse = Date.parse;
+  Date.parse = function(d) {
+    var result = Date.parseRFC3339(d);
+    if (!result && oldparse) {
+      result = oldparse(d);
+    }
     return result;
   }
-
-  /*
-   * Date.prototype.toRFC3339UTCString
-   * Date instance method to format the date as ISO8601 / RFC 3339 string (in UTC format).
-   * Usage: var d = new Date().toRFC3339UTCString();   =>   "2010-07-25T11:51:31.427Z"
-   * Parameters:
-   *  supressFormating : if supplied and 'true', will force to remove date/time separators
-   *  supressMillis : if supplied and 'true', will force not to include milliseconds
-   */
-  Date.prototype.toRFC3339UTCString = function(supressFormating, supressMillis) {
-    var dSep = (supressFormating ? '' : '-');
-    var tSep = (supressFormating ? '' : ':');
-    var result = this.getUTCFullYear().toString();
-    result += dSep + (this.getUTCMonth() + 1).toPaddedString(2);
-    result += dSep + this.getUTCDate().toPaddedString(2);
-    result += 'T' + this.getUTCHours().toPaddedString(2);
-    result += tSep + this.getUTCMinutes().toPaddedString(2);
-    result += tSep + this.getUTCSeconds().toPaddedString(2);
-    if ((!supressMillis) && (this.getUTCMilliseconds() > 0)) result += '.' + this.getUTCMilliseconds().toPaddedString(3);
-    return result + 'Z';
-  }
+}
