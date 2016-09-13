a8aaa45ed5a8960c055f9c14ffa716e7ad70492c
kw_m <worcester-moorek2018@york.org>
2016-09-13 03:12:17 +0000

    AutoCommit

--- js/Controllers/subControllerFunctions.js
+++ js/Controllers/subControllerFunctions.js
@@ -1,8 +1,8 @@
 function subControllerFunctions($scope, $location, $mdDialog, $mdMedia, $timeout, $mdSidenav, authorizationService, GoogleDriveService, angularGridInstance) {
-	
-	
+
+
 	$scope.DriveMetadataTemplate = {
-		id:'0B5NVuDykezpkYkNpaGxXWk1rM1U',
+		id: '0B5NVuDykezpkYkNpaGxXWk1rM1U',
 		name: 'Like#{]|[}Flagged(True/False){]|[}["LikerEmail","LikerEmail"]',
 		description: '<html>Description Text</html>{]|[}LinkUrl{]|[}PreviewImageUrl',
 		iconLink: 'https://ssl.gstatic.com/docs/doclist/images/icon_10_generic_list.png',
@@ -11,25 +11,23 @@ function subControllerFunctions($scope, $location, $mdDialog, $mdMedia, $timeout
 		modifiedTime: '2016-09-11T16:50:51.767Z',
 		starred: false,
 		viewedByMe: true,
-		owners: [
-			{
-				displayName: 'Kyle Worcester-Moore',
-				emailAddress: 'worcester-moorek2018@york.org',
-			}
-		],
+		owners: [{
+			displayName: 'Kyle Worcester-Moore',
+			emailAddress: 'worcester-moorek2018@york.org',
+		}],
 		properties: {
 			Title: 'hi',
 			Type: 'noLink',
 			AttachmentId: '',
 			Tag1: '',
-			Tag2:'',
-			ClassCatagory:'Physical Sciences',
-			ClassColor:'#e6f9f4',
-			ClassName:'Physical Science (8th)',
+			Tag2: '',
+			ClassCatagory: 'Physical Sciences',
+			ClassColor: '#e6f9f4',
+			ClassName: 'Physical Science (8th)',
 		}
 	}
-	
-		$scope.PostTemplate = {
+
+	$scope.PostTemplate = {
 		Title: '',
 		Description: '',
 		Link: '',
@@ -56,25 +54,52 @@ function subControllerFunctions($scope, $location, $mdDialog, $mdMedia, $timeout
 		PreviewImage: '',
 		Bookmarked: false,
 	}
-	
+
 	//----------------------------------------------------
 	//------------------ Converting ----------------------
 	$scope.convertDriveToPost = function(DriveMetadata) {
-		var formatedPost = $scope.PostTemplate;
 		var likesAndFlagged = DriveMetadata.name.split("{]|[}");
 		var descriptionAndPreviewimage = DriveMetadata.description.split("{]|[}");
-		
+
+		var formatedPost = {
+			Title: DriveMetadata.properties.Title || '',
+			Description: '',
+			Link: descriptionAndPreviewimage[1] || '',
+			Tags: JSON.parse("[\"" + DriveMetadata.properties.Tag1 + DriveMetadata.properties.Tag2 + "\"]"),
+			Type: DriveMetadata.properties.Type || 'noLink',
+			Flagged: false,
+			CreationDate: new Date(),
+			UpdateDate: new Date(),
+			Class: {
+				Name: '',
+				Catagory: '',
+				Color: '#ffffff',
+			},
+			Creator: {
+				ClassOf: '',
+				Email: '',
+				Me: null,
+				Name: '',
+			},
+			Link: '',
+			Id: '',
+			AttachmentId: '',
+			Likes: [],
+			PreviewImage: '',
+			Bookmarked: false,
+		}
+
 		DriveMetadata.Title = DriveMetadata.properties.Title;
 		DriveMetadata.Type = DriveMetadata.properties.Type;
 		DriveMetadata.Description = descriptionAndPreviewimage[0];
 		DriveMetadata.Link = descriptionAndPreviewimage[1];
 		DriveMetadata.PreviewImage = descriptionAndPreviewimage[2];
 		DriveMetadata.Tags = JSON.parse("[\"" + DriveMetadata.properties.Tag1 + DriveMetadata.properties.Tag2 + "\"]");
-		DriveMetadata.Likes = JSON.parse(likesAndFlagged[2]);//like email array
-		DriveMetadata.Flagged = likesAndFlagged[2],
-		CreationDate: new Date(),
-		UpdateDate: new Date(),
-		
+		DriveMetadata.Likes = JSON.parse(likesAndFlagged[2]); //like email array
+		DriveMetadata.Flagged = likesAndFlagged[1];
+		DriveMetadata.CreationDate: Date.parse(DriveMetadata.createdTime),
+			DriveMetadata.UpdateDate: Date.parse(DriveMetadata.modifiedTime),
+
 	};
 	$scope.convertPostToDrive = function(Post) {
 		var formatedDriveMetadata = {};
