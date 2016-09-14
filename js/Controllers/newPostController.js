bf7286b8d174ff4218f0750da65c64242b1c0e3b
kw_m <worcester-moorek2018@york.org>
2016-09-14 02:14:15 +0000

    AutoCommit

--- js/Controllers/newPostController.js
+++ js/Controllers/newPostController.js
@@ -1,7 +1,6 @@
     /* we don't define the "new post controller" here because it was alredy
                                                            defined by the $md-dialog in the newPost function on mainController.   */
     function newPostController($scope, $mdDialog, GoogleDriveService, $mdToast, postObj, operation) {
-        console.log(postObj);
         //database variables
         // $scope.Post.Type = 'noLink';
         // $scope.Flagged = false;
