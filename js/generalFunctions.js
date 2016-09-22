1e9ef64b0a49b2cf15ffea1ab83b6ab958f2169e
kw_m <worcester-moorek2018@york.org>
2016-09-22 02:54:38 +0000

    AutoCommit

--- js/generalFunctions.js
+++ js/generalFunctions.js
@@ -89,31 +89,6 @@
     }
   }
 
-  function makeRequest(method, url) {
-    return new Promise(function(resolve, reject) {
-      var xhr = new XMLHttpRequest();
-      xhr.open(method, url);
-      xhr.onload = function() {
-        if (this.status >= 200 && this.status < 300) {
-          resolve(xhr.response);
-        }
-        else {
-          reject({
-            status: this.status,
-            statusText: xhr.statusText
-          });
-        }
-      };
-      xhr.onerror = function() {
-        reject({
-          status: this.status,
-          statusText: xhr.statusText
-        });
-      };
-      xhr.send();
-    });
-  }
-
   function addDays(date, days) {
     var result = new Date(date);
     result.setDate(result.getDate() + days);
