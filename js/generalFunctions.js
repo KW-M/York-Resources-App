fd3461fed468bd5a19545d48ebf020defc428df8
kw_m <worcester-moorek2018@york.org>
2016-09-23 02:32:06 +0000

    AutoCommit

--- js/generalFunctions.js
+++ js/generalFunctions.js
@@ -88,6 +88,14 @@
 
     }
   }
+  
+  function decriptURL (Input) {
+    input
+    atob(str)
+      return decodeURIComponent(Array.prototype.map.call(, function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+      }).join(''));
+  }
 
   function addDays(date, days) {
     var result = new Date(date);
