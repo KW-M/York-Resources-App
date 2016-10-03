54876c430de6cf9842c4bb7b4e165f844416d146
kw_m <worcester-moorek2018@york.org>
2016-10-03 22:45:37 +0000

    AutoCommit

--- js/generalFunctions.js
+++ js/generalFunctions.js
@@ -14,6 +14,8 @@
     }
 
   }
+  
+  function (getURL)
 
   // Returns a function, that, as long as it continues to be invoked, will not
   // be triggered. The function will be called after it stops being called for
