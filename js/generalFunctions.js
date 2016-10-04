23c9cdc8dfb371a79089f61d2116218b42932db4
kw_m <worcester-moorek2018@york.org>
2016-10-04 18:32:14 +0000

    AutoCommit

--- js/generalFunctions.js
+++ js/generalFunctions.js
@@ -36,19 +36,23 @@
   // be triggered. The function will be called after it stops being called for
   // N milliseconds. If `immediate` is passed, trigger the function on the
   // leading edge, instead of the trailing.
-  function debounce(func, wait, immediate) {
+  var debounce = function(func, wait, immediate) {
     var timeout;
     return function() {
       var context = this,
         args = arguments;
       var later = function() {
         timeout = null;
-        if (!immediate) func.apply(context, args);
+        if (!immediate) {
+          func.apply(context, args);
+        }
       };
       var callNow = immediate && !timeout;
       clearTimeout(timeout);
-      timeout = setTimeout(later, wait);
-      if (callNow) func.apply(context, args);
+      timeout = setTimeout(later, wait || 200);
+      if (callNow) {
+        func.apply(context, args);
+      }
     };
   };
 
