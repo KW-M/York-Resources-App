2763faf5f6f38bd07b16e2a0d9bdb27133670c4d
kw_m <worcester-moorek2018@york.org>
2016-10-08 00:12:06 +0000

    AutoCommit

--- js/Controllers/newPostController.js
+++ js/Controllers/newPostController.js
@@ -123,8 +123,13 @@
                 template: '<md-toast><span style="font-size:18px; max-width: 200px">Posting...</span><span flex></span><md-progress-circular class="md-accent" md-mode="indeterminate" style="margin-right: -12px;" md-diameter="36"></md-progress-circular></md-toast>',
                 hideDelay: 3000000,
             });
-            document.dispatchEvent(new window.Event('urlPreviewLoaded'));
-            $scope.submit();
+            if($scope.previewLoading){
+                document.addEventListener('urlPreviewLoaded', function() {
+                    $scope.submit();
+                });
+            } else {
+                $scope.submit();
+            }
         };
 
         $scope.submit = function() {
