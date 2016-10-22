59d8204672a3f42e0a6f7c845da130c92fa1457f
kw_m <worcester-moorek2018@york.org>
2016-10-22 22:28:51 +0000

    AutoCommit

--- js/generalFunctions.js
+++ js/generalFunctions.js
@@ -235,3 +235,48 @@
       return result;
     }
   }
+  
+  
+
+
+var caesarShift = function(str, amount) {
+  //Call it like this: caesarShift('Attack at dawn!', 12); Returns "Mffmow mf pmiz!"
+  // And reverse it like this: caesarShift('Mffmow mf pmiz!', -12); Returns "Attack at dawn!"
+	// Wrap the amount
+	if (amount < 0)
+		return caesarShift(str, amount + 26);
+
+	// Make an output variable
+	var output = '';
+
+	// Go through each character
+	for (var i = 0; i < str.length; i ++) {
+
+		// Get the character we'll be appending
+		var c = str[i];
+
+		// If it's a letter...
+		if (c.match(/[a-z]/i)) {
+
+			// Get its code
+			var code = str.charCodeAt(i);
+
+			// Uppercase letters
+			if ((code >= 65) && (code <= 90))
+				c = String.fromCharCode(((code - 65 + amount) % 26) + 65);
+
+			// Lowercase letters
+			else if ((code >= 97) && (code <= 122))
+				c = String.fromCharCode(((code - 97 + amount) % 26) + 97);
+
+		}
+
+		// Append
+		output += c;
+
+	}
+
+	// All done!
+	return output;
+
+};
