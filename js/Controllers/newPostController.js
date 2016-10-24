0294e11f72077781c37ceda5a2dbfb80ca7cc3e6
kw_m <worcester-moorek2018@york.org>
2016-10-24 01:21:59 +0000

    AutoCommit

--- js/Controllers/newPostController.js
+++ js/Controllers/newPostController.js
@@ -1,7 +1,7 @@
     /* we don't define the "new post controller" here because it was alredy
-                                                                                               defined by the $md-dialog in the newPost function on mainController.   */
+                                                                                                   defined by the $md-dialog in the newPost function on mainController.   */
     function newPostController($scope, $timeout, $http, $mdDialog, GoogleDriveService, $mdToast, postObj, operation) {
-       var linkChangeTimer = null;
+        var linkChangeTimer = null;
         $timeout(function() {
             $scope.Post = {
                 Title: postObj.Title || '',
@@ -72,41 +72,63 @@
                     });
                 } else {
                     $scope.Post.Type = 'Link';
-                    $http.head('https://jsonp.afeld.me/?url=' + $scope.Post.Link).then(function(result){
-                        console.log(result)
-                        console.log(result.headers)
-                    },function(error) {
-                        console.log(error)
-                    })
-                    // request.open('HEAD', 'https://jsonp.afeld.me/?url=' + $scope.Post.Link, true); // to implement: img checking and icon for non existant thumnail drive docs
-                    // request.onLoad = function() {
-                    //     if (this.readyState == 4 && this.status == 200) {
-                    //         console.log(this)
-                    //         var type = this.getResponseHeader('content-type')
-                    //         if (this.getResponseHeader('content-type').indexOf('image') != -1) {
-                    //             $timeout(function() {
-                    //                 $scope.Post.PreviewImage = $scope.Post.Link;
-                    //                 $scope.previewLoading = false;
-                    //                 document.dispatchEvent(new window.Event('urlPreviewLoaded'));
-                    //             })
-                    //         } else {
-                    //             GoogleDriveService.getWebsiteScreenshot($scope.Post.Link).then(function(response) {
-                    //                 $timeout(function() {
-                    //                     $scope.Post.PreviewImage = "data:image/jpeg;base64," + response.result.screenshot.data.replace(/_/g, '/').replace(/-/g, '+');
-                    //                     $scope.previewLoading = false;
-                    //                     document.dispatchEvent(new window.Event('urlPreviewLoaded'));
-                    //                 })
-                    //             })
-                    //         }
-                    //     } else {
-                    //             GoogleDriveService.getWebsiteScreenshot($scope.Post.Link).then(function(response) {
-                    //                 $timeout(function() {
-                    //                     $scope.Post.PreviewImage = "data:image/jpeg;base64," + response.result.screenshot.data.replace(/_/g, '/').replace(/-/g, '+');
-                    //                     $scope.previewLoading = false;
-                    //                     document.dispatchEvent(new window.Event('urlPreviewLoaded'));
-                    //                 })
-                    //             },function(){
-                                    
+                    $http.head('https://jsonp.afeld.me/?url=' + $scope.Post.Link).then(function(result) {
+                            console.log(result)
+                            if (result.headers()['content-type'].indexOf('image') != -1) {
+                                $timeout(function() {
+                                    $scope.Post.PreviewImage = $scope.Post.Link;
+                                    $scope.previewLoading = false;
+                                    document.dispatchEvent(new window.Event('urlPreviewLoaded'));
+                                })
+                            } else {
+
+                            }
+                        }, function(error) {
+                            console.log(error)
+                            GoogleDriveService.getWebsiteScreenshot($scope.Post.Link).then(function(response) {
+                                $timeout(function() {
+                                    $scope.Post.PreviewImage = "data:image/jpeg;base64," + response.result.screenshot.data.replace(/_/g, '/').replace(/-/g, '+');
+                                    $scope.previewLoading = false;
+                                    document.dispatchEvent(new window.Event('urlPreviewLoaded'));
+                                })
+                            }, function(error) {
+                                console.log(error)
+                                $timeout(function() {
+                                    $scope.Post.PreviewImage = "data:image/jpeg;base64," + response.result.screenshot.data.replace(/_/g, '/').replace(/-/g, '+');
+                                    $scope.previewLoading = false;
+                                    document.dispatchEvent(new window.Event('urlPreviewLoaded'));
+                                })
+                            })
+                        })
+                        // request.open('HEAD', 'https://jsonp.afeld.me/?url=' + $scope.Post.Link, true); // to implement: img checking and icon for non existant thumnail drive docs
+                        // request.onLoad = function() {
+                        //     if (this.readyState == 4 && this.status == 200) {
+                        //         console.log(this)
+                        //         var type = this.getResponseHeader('content-type')
+                        //         if (this.getResponseHeader('content-type').indexOf('image') != -1) {
+                        //             $timeout(function() {
+                        //                 $scope.Post.PreviewImage = $scope.Post.Link;
+                        //                 $scope.previewLoading = false;
+                        //                 document.dispatchEvent(new window.Event('urlPreviewLoaded'));
+                        //             })
+                        //         } else {
+                        //             GoogleDriveService.getWebsiteScreenshot($scope.Post.Link).then(function(response) {
+                        //                 $timeout(function() {
+                        //                     $scope.Post.PreviewImage = "data:image/jpeg;base64," + response.result.screenshot.data.replace(/_/g, '/').replace(/-/g, '+');
+                        //                     $scope.previewLoading = false;
+                        //                     document.dispatchEvent(new window.Event('urlPreviewLoaded'));
+                        //                 })
+                        //             })
+                        //         }
+                        //     } else {
+                        //             GoogleDriveService.getWebsiteScreenshot($scope.Post.Link).then(function(response) {
+                        //                 $timeout(function() {
+                        //                     $scope.Post.PreviewImage = "data:image/jpeg;base64," + response.result.screenshot.data.replace(/_/g, '/').replace(/-/g, '+');
+                        //                     $scope.previewLoading = false;
+                        //                     document.dispatchEvent(new window.Event('urlPreviewLoaded'));
+                        //                 })
+                        //             },function(){
+
                     //             }) 
                     //     }
                     // }
@@ -142,12 +164,12 @@
                 $scope.Post.Type = 'NoLink';
             }
         };
-        
-        $scope.$watch('Post.Link',function(){
-            if(typeof(linkChangeTimer) == 'number') {
+
+        $scope.$watch('Post.Link', function() {
+            if (typeof(linkChangeTimer) == 'number') {
                 clearTimeout(linkChangeTimer);
             }
-		    linkChangeTimer = setTimeout($scope.findType())
+            linkChangeTimer = setTimeout($scope.findType())
         })
 
         $scope.isReadyToSubmit = function() {
@@ -218,10 +240,10 @@
             }
         }
 
-$scope.clearLink = function() {
-    $scope.Post.Link = ""
-    $scope.Post.Type = "NoLink"
-}
+        $scope.clearLink = function() {
+            $scope.Post.Link = ""
+            $scope.Post.Type = "NoLink"
+        }
 
         $scope.closeDialog = function() {
             $mdDialog.hide();
