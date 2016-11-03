7315ec5b1f0e330d925cf9195dfccd5dd7b1d47f
kw_m <worcester-moorek2018@york.org>
2016-11-03 03:11:24 +0000

    AutoCommit

--- js/Controllers/mainControllerFunctions.js
+++ js/Controllers/mainControllerFunctions.js
@@ -274,9 +274,6 @@ function controllerFunction($scope, $rootScope, $mdDialog, $window, $http, $time
    //----------------------------------------------------
    //--------------- Creating Posts ---------------------
    $scope.newPost = function(postObj, operation, event) {
-      postObj = {
-         Title:'ready'
-      }
       $mdDialog.show({
          templateUrl: '/directives/html/newPostContent.html',
          controller: ['$scope', '$timeout', '$http', '$mdDialog', 'GoogleDriveService', '$mdToast', "postObj", "operation", newPostController],
