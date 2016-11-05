4d9582aac452a62883bc617d9387ad2ee10903ae
kw_m <worcester-moorek2018@york.org>
2016-11-05 00:25:11 +0000

    AutoCommit

--- js/Controllers/newPostController.js
+++ js/Controllers/newPostController.js
@@ -175,11 +175,13 @@
                     });
                 } else {
                     if ($scope.Post.Type === "gDrive") {
-                        $mdToast.show({
-                            template: '<md-toast style="width: 100%;"><div style="flex-direction: column; height: 100%;" class="md-toast-content"><p style="margin-top:10px">Anyone at York will be able to view the attached file.</p><span flex layout="row" style="width:100%"><md-button style="width:100%" ng-click="checkHeaderImg()">Got It</md-button></span><div></md-toast>',
-                            hideDelay: 3000000,
-                            parent: document.getElementById('new_post_dialog'),
-                        });
+                        // $mdToast.show({
+                        //     template: '<md-toast style="width: 100%;"><div style="flex-direction: column; height: 100%;" class="md-toast-content"><p style="margin-top:10px">Anyone at York will be able to view the attached file.</p><span flex layout="row" style="width:100%"><md-button style="width:100%" ng-click="checkHeaderImg()">Got It</md-button></span><div></md-toast>',
+                        //     hideDelay: 3000000,
+                        //     parent: document.getElementById('new_post_dialog'),
+                        // });
+                        
+                    $mdToast.show($mdToast.simple().action('Got It').textContent('Anyone at York will be able to view the attached file.'));
                     } else {
                         $mdToast.show({
                             template: '<md-toast><span style="font-size:18px; max-width: 200px">Posting...</span><span flex></span><md-progress-circular class="md-accent" md-mode="indeterminate" style="margin-right: -12px;" md-diameter="36"></md-progress-circular></md-toast>',
