5a3b68e556ac73b737b9dac987b3ccbfa3d3bf18
kw_m <worcester-moorek2018@york.org>
2016-11-11 03:34:38 +0000

    AutoCommit

--- directives/directives.js
+++ directives/directives.js
@@ -105,30 +105,23 @@ app.directive('contenteditable', ['$sce', function ($sce) {
         require: '?ngModel', // get a hold of NgModelController
         link: function (scope, element, attrs, ngModel) {
             if (!ngModel) return; // do nothing if no ng-model
-            //     console.log('viewModel0:')
-            //       console.log(ngModel)
-            //       console.log(scope.Post.Description)
-            //   // Update UI on first run
-            //   element.html($sce.getTrustedHtml(scope.Post.Description || ''));
 
             // Specify how UI should be updated
-            // scope.$timeout(function () {
-            //     ngModel.$render = function () {
-            //         console.log('viewModel:')
-            //         console.log(ngModel.$viewValue)
-            //         element.html($sce.getTrustedHtml(ngModel.$viewValue || ''));
-            //     };
-            // },5000)
-
+            ngModel.$render = function () {
+                console.log('viewModel:')
+                console.log(ngModel.$viewValue)
+                element.html($sce.getTrustedHtml(ngModel.$viewValue || ''));
+            };
 
             // Listen for change events to enable binding
             element.on('blur keyup change', function () {
                 scope.$evalAsync(read);
             });
-            read(); // initialize
+            scope.$timeout(read); // initialize
 
             // Write data to the model
             function read() {
+
                 var html = element.html();
                 // When we clear the content editable the browser leaves a <br> behind
                 // If strip-br attribute is provided then we strip this out
