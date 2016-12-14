4e073ccc07fa6767e7cc6a5355d252842b4fb0c0
kw_m <worcester-moorek2018@york.org>
2016-12-14 17:33:55 +0000

    AutoCommit

--- js/Controllers/mainControllerFunctions.js
+++ js/Controllers/mainControllerFunctions.js
@@ -251,11 +251,11 @@ function controllerFunction($scope, $rootScope, $filter, $mdDialog, $mdToast, $w
             return GoogleDriveService.appendSpreadsheetRange([$scope.myInfo.Email, $scope.myInfo.Name, 0, 0, "", "", "", ""]);
          }).then(function(userSpreadsheetRow) {
             console.log(userSpreadsheetRow)
-            userSpreadsheetRow.result.values[0][4]++;
-            userSpreadsheetRow.result.values[0].shift()
-            
-            $scope.convertRowToUserPreferences(userSpreadsheetRow.result.values[0]);
-            return GoogleDriveService.updateSpreadsheetRange("Sheet1!C" + $scope.UserSettingsRowNum,userSpreadsheetRow.result.values[0])
+            // userSpreadsheetRow.result.values[0][4]++;
+            // userSpreadsheetRow.result.values[0].shift()
+            // userSpreadsheetRow.result.values[0].shift()
+            //$scope.convertRowToUserPreferences(userSpreadsheetRow.result.values[0]);
+            return GoogleDriveService.updateSpreadsheetRange("Sheet1!C" + $scope.UserSettingsRowNum + ":C",userSpreadsheetRow.result.values[0])
          }, function(error) {
             console.warn(error)
          }).then(function(updatedUserSpreadsheetRow) {
