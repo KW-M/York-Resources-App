9ebf00d54ff9b4614453fb13a3148a49018ac2a8
kw_m <worcester-moorek2018@york.org>
2017-01-05 04:03:15 +0000

    AutoCommit

--- js/Controllers/newPostController.js
+++ js/Controllers/newPostController.js
@@ -191,7 +191,7 @@
                 else {
                     if ($scope.Post.Type === "gDrive") {
                         $mdToast.show({
-                            template: '<md-toast> <div class="md-toast-content" style="justify-content: center;"> <div> <div class="md-toast-text" style="padding: 6px 0 0 0px;">How should the attached file be shared?</div> <div style="display:flex"> <md-select ng-model="shareSelect"> <md-option value="view"> York students can view </md-option> <md-option value="comment"> York students can comment </md-option> <md-option value="edit"> York students can edit </md-option> </md-select> <md-button>Share</md-button> </div></div></div></md-toast>',
+                            template: '<md-toast> <div class="md-toast-content" style="justify-content: center;"> <div> <div class="md-toast-text" style="padding: 6px 0 0 0px;">How should the attached file be shared?</div> <div style="display:flex"> <md-select ng-model="shareSelect"> <md-option value="view"> York students can view </md-option> <md-option value="comment"> York students can comment </md-option> <md-option value="edit"> York students can edit </md-option> </md-select> <md-button style="color:rgb(68,138,255)">Share</md-button> </div></div></div></md-toast>',
                             hideDelay: 3000000,
                             parent: document.getElementById('new_post_dialog'),
                             toastClass:'shareLevelToast',
