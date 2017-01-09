1b8fa92504e421577147bffdfe2576209c43ff16
kw_m <worcester-moorek2018@york.org>
2017-01-09 01:49:49 +0000

    AutoCommit

--- js/Controllers/subControllerFunctions.js
+++ js/Controllers/subControllerFunctions.js
@@ -418,7 +418,7 @@ function subControllerFunctions($scope, $location, $mdDialog, $mdToast, $mdMedia
 		})
 	}
 	$scope.moveLabeltoAllLabels = function(activeLabelIndex) {
-		var label = $scope.queryParams.labels.splice(activeLabelIndex, 1)
+		var label = $scope.Post.Labels.splice(activeLabelIndex, 1)
 		$scope.allLabels.push(label[0]);
 		$timeout(function() {
 			$scope.visibleLabels = $scope.sortLabels($scope.allLabels)
