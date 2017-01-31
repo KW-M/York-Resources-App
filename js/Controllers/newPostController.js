9fb81977bc6d9be1ec5f5668db5dcefa645867ab
kw_m <worcester-moorek2018@york.org>
2017-01-31 03:13:36 +0000

    AutoCommit

--- js/Controllers/newPostController.js
+++ js/Controllers/newPostController.js
@@ -125,15 +125,16 @@ function newPostController($scope, $timeout, $http, $mdDialog, APIService, autho
                 console.log(postData)
                 var createdPost = JSON.parse(postData.result.response.result);
                 console.log(createdPost)
-                addFireDatabaseRef(createdPost).then(fun)
-                $mdToast.hide();
-                $mdDialog.hide();
-                $scope.dialog_container.style.opacity = 1;
-                $scope.dialog_container.style.pointerEvents = 'all';
-                // $timeout(function () {
-                //     $scope.allPosts.push($scope.post)
-                //     $scope.visiblePosts = $scope.filterPosts($scope.allPosts);
-                // })
+                addFireDatabaseRef(createdPost).then(function () {
+                    $mdToast.hide();
+                    $mdDialog.hide();
+                    $scope.dialog_container.style.opacity = 1;
+                    $scope.dialog_container.style.pointerEvents = 'all';
+                    // $timeout(function () {
+                    //     $scope.allPosts.push($scope.post)
+                    //     $scope.visiblePosts = $scope.filterPosts($scope.allPosts);
+                    // })
+                })
             },
             onError, 150);
     }
