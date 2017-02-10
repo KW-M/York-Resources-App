361e598d05887291df2aa0814fb673c2bf94bf65
kw_m <worcester-moorek2018@york.org>
2017-02-10 23:14:30 +0000

    AutoCommit

--- js/Controllers/mainControllerFunctions.js
+++ js/Controllers/mainControllerFunctions.js
@@ -677,9 +677,10 @@ function controllerFunction($scope, $rootScope, $window, $timeout, $filter, $q, 
    $scope.confirmDelete = function (content, arrayIndex) {
       var confirm = $mdDialog.confirm().title('Permanently delete this?').ariaLabel('Delete?').ok('Delete').cancel('Cancel');
       $mdDialog.show(confirm).then(function () {
+         $mdToast.showSimple('Deleting...');
          promiseQueue().addPromise('script', APIService.runGAScript('deletePost', content.id, false), function (returnedValue) {
             console.log(returnedValue.result.response.result)
-            if (returnedValue.result.response.result == true) authorizationService.FireDatabase.ref('posts/' + content.id).remove().then(null, console.warn)
+            if (returnedValue.result.response.result == true) authorizationService.FireDatabase.ref('posts/' + content.id).remove().then($mdToast.hide, console.warn)
          }, console.warn, 150);
       });
    };
