d16e0488c7b60168db4bd53bc21e7aeabb4bb950
kw_m <worcester-moorek2018@york.org>
2017-02-11 18:54:15 +0000

    AutoCommit

--- js/Controllers/mainControllerFunctions.js
+++ js/Controllers/mainControllerFunctions.js
@@ -340,6 +340,7 @@ function controllerFunction($scope, $rootScope, $window, $timeout, $filter, $q, 
       conurancyCounter++;
       console.log(idArray)
       promiseQueue().addPromise('script', APIService.runGAScript('getPosts', idArray, false), function (postsData) {
+         console.log(postsData)
          var postsArray = JSON.parse(postsData.result.response.result);
          if (postsArray.error == undefined) {
             conurancyCounter--;
