2933287af1fd3b77cf10285db8ea9b140127d469
kw_m <worcester-moorek2018@york.org>
2017-02-12 06:16:55 +0000

    AutoCommit

--- js/Controllers/newPostController.js
+++ js/Controllers/newPostController.js
@@ -213,6 +213,13 @@ function newPostController($scope, $timeout, $http, $mdDialog, APIService, autho
             $scope.sortedLabels[labelCount].active == false;
         }
     }
+    
+    function hiddeSelectedLabels() {
+        var max = $scope.post.Labels.lengt
+        for (var labelCount = 0; labelCount < max; labelCount++) {
+            $scope.sortedLabels[labelCount].active == false;
+        }
+    }
 
     function onError(error) {
         console.warn(error);
