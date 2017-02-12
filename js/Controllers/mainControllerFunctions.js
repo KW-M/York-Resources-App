e82cb5be095c6a7a1c8cc523e7537457a481cd06
kw_m <worcester-moorek2018@york.org>
2017-02-12 04:31:21 +0000

    AutoCommit

--- js/Controllers/mainControllerFunctions.js
+++ js/Controllers/mainControllerFunctions.js
@@ -200,13 +200,9 @@ function controllerFunction($scope, $rootScope, $window, $timeout, $filter, $q, 
          postsFireRef.on('child_changed', function (childSnapshot, oldSnapshot) {
             console.log(childSnapshot)
             var indexes = getIdIndexInPostArrays(childSnapshot.key)
-                           console.log(indexes)
             var oldPost = $scope.allPosts[indexes.allPosts]
-                           console.log(oldPost)
             var newSlimPost = convertFirePost(childSnapshot.key, childSnapshot.val(), 'Loaded')
-            console.log(newSlimPost)
             var mergedFullPost = mergeFirebasePost(oldPost, newSlimPost);
-            console.log(mergedFullPost)
             if (newSlimPost.updateDate != oldPost.updateDate) {
                mergedFullPost.loadStatus = 'Changed'
                var reSortPosts = true;
