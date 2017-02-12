a90091410266dd1ab2337bb291ff29ca6b167421
kw_m <worcester-moorek2018@york.org>
2017-02-12 03:41:03 +0000

    AutoCommit

--- js/Controllers/mainControllerFunctions.js
+++ js/Controllers/mainControllerFunctions.js
@@ -727,7 +727,8 @@ function controllerFunction($scope, $rootScope, $window, $timeout, $filter, $q, 
             var arrayIndecies = getIdIndexInPostArrays(post.id)
             var res = data.result.response.result.split(" ");
             res[0] = res[0] == 'true'
-            res[1] = parseInt(res[0])
+            res[1] = parseInt(res[1])
+            console.log(res)
             $timeout(function () {
                ($scope.allPosts[arrayIndecies.allPosts] || {}).userLiked = res[0];
                ($scope.allPosts[arrayIndecies.allPosts] || {}).likeCount = res[1];
