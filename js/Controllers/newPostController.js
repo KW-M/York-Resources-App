d504bccbb6f5d7041dfd5cab18f2209e46440098
kw_m <worcester-moorek2018@york.org>
2017-02-22 00:28:01 +0000

    AutoCommit

--- js/Controllers/newPostController.js
+++ js/Controllers/newPostController.js
@@ -160,7 +160,7 @@ function newPostController($scope, $timeout, $http, $mdDialog, APIService, autho
         $scope.dialog_container.style.pointerEvents = 'none';
         console.log('posting toast show')
         // $mdToast.show({
-        //     template: '<md-toast><span style="font-size:18px; max-width: 200px">Posting</span><span flex></span><md-progress-circular class="md-accent" md-mode="indeterminate" style="margin-right: -12px;" md-diameter="32"></md-progress-circular></md-toast>',
+        //     template: '<md-toast><span style="font-size:18px;" flex>Posting</span><md-progress-circular class="md-accent" md-mode="indeterminate" style="margin-right: -12px;" md-diameter="32"></md-progress-circular></md-toast>',
         //     hideDelay: false,
         // });
         console.log('Sent Post:', $scope.post)
