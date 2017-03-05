7a9c9234963e80bd79a1928ffe007ee831f53115
kw_m <worcester-moorek2018@york.org>
2017-03-05 20:32:02 +0000

    AutoCommit

--- js/Controllers/newPostController.js
+++ js/Controllers/newPostController.js
@@ -101,7 +101,7 @@ function newPostController($scope, $timeout, $http, $mdDialog, APIService, autho
                                 document.dispatchEvent(new Event('linkPreviewLoaded'));
                             })
                         }
-                    }, onError, 150, 'Problem showing link preview, is your attachment link an actual URL?');
+                    }, onError, 150, 'Problem showing link preview, is your attachment link a valid URL?',1);
                 } else if ($scope.post.link.length > 4 & $scope.post.link.substring(0,3) != 'htt') {
                     $scope.post.link = "http://" + $scope.post.link;
                     $scope.post.type = 'link';
@@ -130,7 +130,7 @@ function newPostController($scope, $timeout, $http, $mdDialog, APIService, autho
                     scope.shareSelect = 'reader';
                     scope.shareFile = function () {
                         if (scope.shareSelect != 'none') {
-                            promiseQueue.addPromise('drive', APIService.shareFile($scope.post.attachmentId, scope.shareSelect), $scope.submit, $scope.submit, 150, "The attached file couln't be shared, please share it manualy.");
+                            promiseQueue.addPromise('drive', APIService.shareFile($scope.post.attachmentId, scope.shareSelect), $scope.submit, $scope.submit, 150, "The attached file couln't be shared, please share it manualy.",2);
                         } else {
                             $scope.submit()
                         }
@@ -172,7 +172,7 @@ function newPostController($scope, $timeout, $http, $mdDialog, APIService, autho
                     $mdToast.hide();
                 })
             },
-            onError, 150, 'Error posting, try again.');
+            onError, 150, 'Error posting, try again.',);
     }
 
     function addFireDatabaseRef(post) {
