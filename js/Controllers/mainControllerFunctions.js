2ece34ca38ed9401e16f77742e2638efd5acc6db
kw_m <worcester-moorek2018@york.org>
2017-04-05 00:49:10 +0000

    AutoCommit

--- js/Controllers/mainControllerFunctions.js
+++ js/Controllers/mainControllerFunctions.js
@@ -487,7 +487,7 @@ function controllerFunction($scope, $rootScope, $window, $timeout, $filter, $q, 
             for (var count = 0; count < max; count++) {
                console.log('got from gdrive - post #' + count)
                var post = addFullPost(postsArray[count])
-               if (post.class.stared === true) localforage.setItem(post.id, post);
+               if (post.class.stared === true && navigator.webkitTemporaryStorage !== undefined) localforage.setItem(post.id, post);
             }
             $timeout(function () {
                $scope.sortedPosts = $scope.sortedPosts;
