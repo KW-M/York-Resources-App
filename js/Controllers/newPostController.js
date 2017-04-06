79b968dda1ff92add09cf39836d232847159dde4
kw_m <worcester-moorek2018@york.org>
2017-04-06 00:55:54 +0000

    AutoCommit

--- js/Controllers/newPostController.js
+++ js/Controllers/newPostController.js
@@ -135,7 +135,9 @@ function newPostController($scope, $timeout, $http, $mdDialog, APIService, autho
                     scope.shareSelect = 'commenter';
                     scope.shareFile = function () {
                         if (scope.shareSelect != 'none') {
-                            promiseQueue.addPromise('drive', APIService.shareFile($scope.post.attachmentId, scope.shareSelect), $scope.submit, $scope.submit, 150, "The attached file couln't be shared, please share it manualy.", 1);
+                            promiseQueue.addPromise('drive', APIService.shareFile($scope.post.attachmentId, scope.shareSelect), $scope.submit, function () {
+                                setTimeout($scope.submit, 000);
+                            }, 150, "The attached file couldn't be shared, please share it manualy.", 2);
                         } else {
                             $scope.submit()
                         }
